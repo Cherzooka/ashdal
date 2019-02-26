228ccb6b3e7e0620c390bb4b795908008b0f3da8
Cherzooka <lady.cherette@gmail.com>
2019-02-26 13:41:48 -0600

    bb
    bb

---
+++ js/index.js
@@ -0,0 +1,24 @@
+window.onload = function () {
+    var app = new Vue({
+        el: '#app',
+        data: {
+            title: 'Eirwen Rhys Sayer',
+
+            biography: 'The eldest child of Briallen Sayer, Eirwen, and his twin sister Gwyneira, were raised in the small farming village of Beaumaris by their grandparents, Rhys and Olivia Sayer. The two were raised for only two years by their mother, with no knowledge of their father. Their village life was largely stiffling and solitary, being the only non-human children of the small village. At 19, Eirwen was conscripted into the local militia, despite his sister\'s attempts to stop the conscription. Only a few months after completing his basic training, Eirwen deserted. Upon returning home, he learned of his sister leaving on her own, and decided to follow suit himself, taking to a life of travel as well. <br /> <br /> Since beginning his life of traveling and adventure, Eirwen has taken a strong interest in learning more about his magical origins, presently believing it to be elven or fey in origin. In his exploration of fey ruins and elven lands, he always keeps a watchful ear open for news of either his sister, or mother. <br /> <br /> During his adventures, he has also befriended and pacted with an fey being, whom he has given the name Caern\'on, after a forest spirit that was believed to live near his hometown. Caern\'on generally travels alongside Eirwen, commonly taking the form of a black raven, fox or wolf.',
+            // Attributes
+            strength: 8,
+            dexterity: 12,
+            constitution: 12,
+            intelligence: 15,
+            wisdom: 12,
+            charisma: 17,
+            
+            circle: '100 60 151.962 70 151.962 130 100 175 48.0385 130 26.3878 57.5',
+
+            
+                      
+                      
+                      
+        }
+    })
+}
