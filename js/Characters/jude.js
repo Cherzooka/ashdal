236fdae3427277e9ab6fc00afdfefa89751268e5
Cherzooka <lady.cherette@gmail.com>
2023-09-30 13:51:03 -0500

    Update jude.js

--- js/Characters/jude.js
+++ js/Characters/jude.js
@@ -3,4 +3,41 @@ characters.push({
     name: "Judicaël de la Fontaine",
     color: "183, 58%, 41%",
     img: "img/jude.png",
+
+        biography: 'text',
+
+    // Details
+    altnames: "-",
+    gender: "Male (he/him)",
+    race: "-",
+    height: "-",
+    age: "-",
+    dob: "-",
+    home: "-",
+    alignment: "-",
+    player: "Cher",
+
+    // Attributes
+    strength: 10,
+    dexterity: 10,
+    constitution: 10,
+    intelligence: 10,
+    wisdom: 10,
+    charisma: 10,
+
+    // Other Statistics
+    multiclass: false,
+    class1: "Wizard",
+    class1level: 1,
+    class2: "-",
+    class2level: 0,
+    proficiencies: "Athletics, Arcana, Perception, Survival, Deception, Persuasion",
+
+
+    // Abilities
+    speed: 30,
+
+    // Miscellaneous
+    unarmoredbonus: false,
+    rhysCaernbonus: false,
 })
